d0433991020af5b6203c530e040e320280c3a97e
watsonjph <corsegajoseph@gmail.com>
2025-07-18 02:43:36 +0800

    FIIXXXXXXXXX

--- server/app.js
+++ server/app.js
@@ -48,7 +48,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
--- server/app.prod.js
+++ server/app.prod.js
@@ -50,6 +50,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Production server running on http://localhost:${PORT}`);
 }); 
